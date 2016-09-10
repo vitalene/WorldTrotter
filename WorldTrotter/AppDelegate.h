38941081609dc9eb9eedb72a40d6df049920944f
Neil <vitalene@msu.edu>
2016-09-10 17:22:00 -0400

    Initial Commit

---
+++ WorldTrotter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WorldTrotter
+//
+//  Created by Neil Vitale on 9/10/16.
+//  Copyright © 2016 Neil Vitale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WorldTrotter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WorldTrotter
+//
+//  Created by Neil Vitale on 9/10/16.
+//  Copyright © 2016 Neil Vitale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
