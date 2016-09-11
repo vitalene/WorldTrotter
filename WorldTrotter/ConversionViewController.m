57d6ebc03fdd1b2b73048a1ab75481368f490061
Neil <vitalene@msu.edu>
2016-09-10 20:04:17 -0400

    mimicking set up

--- WorldTrotter/ConversionViewController.m
+++ WorldTrotter/ConversionViewController.m
@@ -3,10 +3,14 @@
 #import "ConversionViewController.h"
 
 @interface ConversionViewController ()
-
+@property (nonatomic) IBOutlet UILabel *celsiusLabel;
 @end
 
 @implementation ConversionViewController
+
+- (IBAction)fahrenheitFieldEditingChanged:(UITextField *)textField {
+    self.celsiusLabel.text = textField.text;
+}
 
 -(void)viewDidLoad {
 
