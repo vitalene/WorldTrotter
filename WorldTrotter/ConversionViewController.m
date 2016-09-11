da462fda89ddc87cb54a4c179b8558a773f7a757
Neil <vitalene@msu.edu>
2016-09-10 21:15:10 -0400

    updated to do celsius conversion

--- WorldTrotter/ConversionViewController.m
+++ WorldTrotter/ConversionViewController.m
@@ -4,16 +4,42 @@
 
 @interface ConversionViewController ()
 @property (nonatomic) IBOutlet UILabel *celsiusLabel;
+@property (nonatomic) IBOutlet UITextField *fahrenheitField;
+@property (nonatomic) double fahrenheitValue;
+@property (nonatomic) double celsiusValue;
 @end
 
 @implementation ConversionViewController
 
+-(void)setFahrenheitValue:(double)fahrenheitValue {
+    _fahrenheitValue = fahrenheitValue;
+    [self updateCelsiusLabel];
+}
+
+-(void)setCelsiusValue:(double)celsiusValue {
+    self.fahrenheitValue = celsiusValue * (9.0/5.0) + 32;
+}
+
+-(double)celsiusValue {
+    return (self.fahrenheitValue -32) * (5.0/9.0);
+}
+
+-(void)updateCelsiusLabel {
+    self.celsiusLabel.text = @(self.celsiusValue).stringValue;
+}
+
 - (IBAction)fahrenheitFieldEditingChanged:(UITextField *)textField {
-    self.celsiusLabel.text = textField.text;
+
+    double value = textField.text.doubleValue;
+    self.fahrenheitValue = value;
+}
+
+- (IBAction)dismissKeyboard:(id)sender {
+    [self.fahrenheitField resignFirstResponder];
 }
 
 -(void)viewDidLoad {
-
+    
     
 }
 @end
